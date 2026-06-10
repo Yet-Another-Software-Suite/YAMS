86f8e395731fcf8dc3517f2181c457d123e9e69d
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2026-06-10 15:27:46 -0500

    Fix unit tests

--- vendordep/src/test/native/cpp/main.cpp
+++ vendordep/src/test/native/cpp/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2026 Yet Another Software Suite
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+#include <hal/HAL.h>
+
 #include "gtest/gtest.h"
 #include "helpers/ExceptionTracer.h"
 
@@ -8,5 +10,7 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   // Print demangled stack traces whenever a test fails due to an exception.
   ::testing::UnitTest::GetInstance()->listeners().Append(new yams::test::ExceptionTracerListener{});
-  return RUN_ALL_TESTS();
+  int result = RUN_ALL_TESTS();
+  HAL_Shutdown();
+  return result;
 }
